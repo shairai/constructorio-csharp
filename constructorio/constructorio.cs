8e50b01a105db51bfc190ac0f0c5fb92775d0802
Howon Lee <howon@howonlee.com>
2015-10-20 14:32:44 -0700

    first commit

---
+++ constructorio/constructorio.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace ConstructorIOClient {
+  class ConstructorIO {
+    static void Main(string[] args) {
+      Console.WriteLine("boinka");
+      Console.ReadKey();
+    }
+  }
+}
---
+++ readme.md
@@ -0,0 +1,1 @@
+only developed on mono
