76653967ac1c19cd1f04b83ee29329f26cfbbcf4
Howon Lee <howon@howonlee.com>
2015-10-26 16:12:44 -0700

    fix linking bug

---
+++ build_test.sh
@@ -0,0 +1,9 @@
+# probably supposed to use NAnt or something
+# actually, build and run
+
+rm ./test/constructortests.dll
+mcs -t:library -r:./constructorio/constructorio.dll -r:./test/Newtonsoft.Json.7.0.1/lib/net40/Newtonsoft.Json.dll -r:/usr/lib/mono/gac/nunit.framework/2.6.0.0__96d09a1eb7f44a77/nunit.framework.dll ./test/constructortests.cs
+
+echo "built"
+
+nunit-console ./test/constructortests.dll
--- test/build_test.sh
+++
@@ -1,5 +0,0 @@
-# probably supposed to use NAnt or something
-# actually, build and run
-
-mcs -t:library -r:../constructorio/constructorio.dll -r:/usr/lib/mono/gac/nunit.framework/2.6.0.0__96d09a1eb7f44a77/nunit.framework.dll constructortests.cs
-nunit-console ./constructortests.dll
